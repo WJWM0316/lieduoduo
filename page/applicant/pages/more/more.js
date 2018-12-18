eb28c942a11ba739cd9fc96dcf81d8fdc114964d
ChenHaoBin <531120903@qq.com>
2018-12-18 17:23:32 +0800

    合并

--- page/applicant/pages/more/more.js
+++ page/applicant/pages/more/more.js
@@ -1,44 +1,44 @@
 // page/applicant/pages/more/more.js
-Component({
+import { getJobLabelApi, getCityLabelApi } from '../../../../api/pages/common'
+const app = getApp()
+Page({
   /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
+   * 初始数据
    */
   data: {
-    tab: "all",
+    tab: 'all',
     childTabList:['广州','深圳','上海','北京','杭州','重庆','合肥'],
     nowIndex: 0,
     jobLabel: [],
     cityLabel: ['广州', '深圳', '上海', '北京', '杭州', '重庆', '合肥']
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    cutover(event) {
-      this.setData({
-        tab: event.target.dataset.tab
-      })
-    },
-    /* 子级tab栏切换 */
-    toggle(event) {
-      this.setData({
-        nowIndex: event.target.dataset.nowindex
-      })
-    },
-    scroll (e) {},
-    /* 翻页 */
-    loadNext (e) {
-//    console.log(e, '1111111111111')
-    }
+  /* 切换主tab */
+  cutover(event) {
+    this.setData({
+      tab: event.target.dataset.tab
+    })
+  },
+  /* 子级tab栏切换 */
+  toggle(event) {
+    this.setData({
+      nowIndex: event.target.dataset.nowindex
+    })
+  },
+  scroll (e) {},
+  /* 翻页 */
+  loadNext (e) {
+    console.log(e, '1111111111111')
   },
   /* 标签初始化 */
-  getLabelList () {}
+  getJobLabelList () {
+    return getJobLabelApi()
+  },
+  getCityLabel () {
+    return getCityLabelApi()
+  },
+  onShow () {
+    Promise.all([this.getCityLabel(), this.getJobLabelList()]).then((res1, res2) => {
+      console.log(res1, res2, '999999')
+    })
+  }
 })
