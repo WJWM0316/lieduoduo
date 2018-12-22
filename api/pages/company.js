07641a226d0423045ac693ea2d10e8750bd280b2
lizifa <lphvali@thetiger.com.cn>
2018-12-22 11:26:31 +0800

    加公司模块的api

--- api/pages/company.js
+++ api/pages/company.js
@@ -39,3 +39,136 @@ export const identityCompanyApi = (data, isLoading) => {
     isLoading: false
   })
 }
+
+// 创建公司
+export const createCompanyApi = (data, isLoading) => {
+  return request({
+    method: 'post',
+    url: '/company',
+    data,
+    isLoading: false
+  })
+}
+
+// 获取公司信息
+export const getCompanyInfosApi = (data, isLoading) => {
+  return request({
+    method: 'get',
+    url: `/company/${data.id}`,
+    isLoading: false
+  })
+}
+
+// 编辑公司信息
+export const editCompanyInfosApi = (data, isLoading) => {
+  return request({
+    method: 'put',
+    url: `/company/${data.id}`,
+    data,
+    isLoading: false
+  })
+}
+
+// 删除公司
+export const deleteCompanyApi = (data, isLoading) => {
+  return request({
+    method: 'delete',
+    url: `/company/${data.id}`,
+    isLoading: false
+  })
+}
+
+// 通过公司名判断公司是否存在
+export const JustifyCompanyExistApi = (data, isLoading) => {
+  return request({
+    method: 'get',
+    url: `/company/${data.name}`,
+    isLoading: false
+  })
+}
+
+// 编辑公司相册
+export const editCompanyAlbumApi = (data, isLoading) => {
+  return request({
+    method: 'put',
+    url: `/company/${data.name}`,
+    isLoading: false
+  })
+}
+
+// 添加公司地址
+export const addCompanyAddressApi = (data, isLoading) => {
+  return request({
+    method: 'put',
+    url: `/company/${data.id}`,
+    data,
+    isLoading: false
+  })
+}
+
+// 创建公司产品
+export const createCompanyProductApi = (data, isLoading) => {
+  return request({
+    method: 'post',
+    url: '/company/product',
+    data,
+    isLoading: false
+  })
+}
+
+// 获取公司产品信息
+export const getCompanyProductInfosApi = (data, isLoading) => {
+  return request({
+    method: 'get',
+    url: `/company/product/${data.id}`,
+    isLoading: false
+  })
+}
+
+// 编辑公司产品信息
+export const editCompanyProductInfosApi = (data, isLoading) => {
+  return request({
+    method: 'put',
+    url: `/company/product/${data.id}`,
+    data,
+    isLoading: false
+  })
+}
+
+// 删除公司产品信息
+export const deleteCompanyProductInfosApi = (data, isLoading) => {
+  return request({
+    method: 'delete',
+    url: `/company/product/${data.id}`,
+    data,
+    isLoading: false
+  })
+}
+
+// 获取身份信息
+export const getCompanyIdentityInfosApi = (data, isLoading) => {
+  return request({
+    method: 'get',
+    url: `/company/identity/${data.id}`,
+    isLoading: false
+  })
+}
+
+// 获取身份信息
+export const editCompanyIdentityInfosApi = (data, isLoading) => {
+  return request({
+    method: 'put',
+    url: `/company/identity/${data.id}`,
+    data,
+    isLoading: false
+  })
+}
+
+// 删除身份信息
+export const deleteCompanyIdentityInfosApi = (data, isLoading) => {
+  return request({
+    method: 'delete',
+    url: `/company/identity/${data.id}`,
+    isLoading: false
+  })
+}
