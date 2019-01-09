7f03f0cfd0d74245fa2f97bfae0d58ef212225fe
lizifa <lphvali@thetiger.com.cn>
2019-01-09 13:53:32 +0800

    一波ui修改

--- page/recruiter/pages/position/category/category.js
+++ page/recruiter/pages/position/category/category.js
@@ -46,7 +46,6 @@ Page({
     const params = e.currentTarget.dataset
     const positionTypeList = this.data.positionTypeList
     positionTypeList[this.data.index1].children.map((field, index) => field.active = index === params.index ? true : false)
-    console.log(positionTypeList)
     this.setData({index2: params.index, positionTypeList})
   },
   /**
