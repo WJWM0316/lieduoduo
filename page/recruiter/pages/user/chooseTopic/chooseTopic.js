84aec9837251242ab50fdd5a53b7620910a57193
lizifa <lphvali@thetiger.com.cn>
2019-01-21 16:18:58 +0800

    一波修改

--- page/recruiter/pages/user/chooseTopic/chooseTopic.js
+++ page/recruiter/pages/user/chooseTopic/chooseTopic.js
@@ -11,22 +11,26 @@ Page({
     isLastPage: false,
     isRequire: false,
     onBottomStatus: 0,
-    pageCount: app.globalData.pageCount
+    pageCount: app.globalData.pageCount,
+    hasReFresh: false
   },
   onLoad(options) {
     this.getTopicList()
   },
   getTopicList(hasLoading = true) {
-    const params = {count: this.data.pageCount, page: this.data.pageNum, hasLoading}
-    getTopicListApi(params)
-      .then(res => {
-        const list = this.data.list.concat(res.data)
-        const isLastPage = res.meta.nextPageUrl ? false : true
-        const pageNum = this.data.pageNum + 1
-        const isRequire = true
-        const onBottomStatus = res.meta.nextPageUrl ? 0 : 2
-        this.setData({list, isLastPage, pageNum, isRequire, onBottomStatus})
-      })
+    return new Promise((resolve, reject) => {
+      const params = {count: this.data.pageCount, page: this.data.pageNum, hasLoading}
+      getTopicListApi(params)
+        .then(res => {
+          const list = this.data.list.concat(res.data)
+          const isLastPage = res.meta.nextPageUrl ? false : true
+          const pageNum = this.data.pageNum + 1
+          const isRequire = true
+          const onBottomStatus = res.meta.nextPageUrl ? 0 : 2
+          this.setData({list, isLastPage, pageNum, isRequire, onBottomStatus})
+          resolve(res)
+        })
+    })
   },
   addTop(e) {
     let item = e.currentTarget.dataset.item
@@ -34,20 +38,33 @@ Page({
       url: `${RECRUITER}user/editDeclaration/editDeclaration?topicId=${item.id}&title=${item.title}`
     })
   },
+  /**
+   * @Author   小书包
+   * @DateTime 2019-01-21
+   * @detail   下拉重新获取数据
+   * @return   {[type]}              [description]
+   */
   onPullDownRefresh(hasLoading = true) {
-    const params = {count: this.data.pageCount, page: 1, hasLoading}
-    getTopicListApi(params)
-      .then(res => {
-        const list = res.data
-        const isLastPage = res.meta.nextPageUrl ? false : true
-        const pageNum = 1
-        const isRequire = true
-        const onBottomStatus = res.meta.nextPageUrl ? 0 : 2
-        this.setData({list, isLastPage, pageNum, isRequire, onBottomStatus}, () => {
-          wx.stopPullDownRefresh()
+    this.setData({pageNum: 1, hasReFresh: true})
+    this.getTopicList(false)
+        .then((res) => {
+          const list = res.data
+          const isLastPage = res.meta.nextPageUrl ? false : true
+          const pageNum = 2
+          const isRequire = true
+          const onBottomStatus = res.meta.nextPageUrl ? 0 : 2
+          this.setData({list, isLastPage, pageNum, isRequire, onBottomStatus}, () => {
+            wx.stopPullDownRefresh()
+            this.setData({hasReFresh: false})
+          })
         })
-      })
   },
+  /**
+   * @Author   小书包
+   * @DateTime 2019-01-21
+   * @detail   触底加载数据
+   * @return   {[type]}   [description]
+   */
   onReachBottom() {
     this.getTopicList()
   }
