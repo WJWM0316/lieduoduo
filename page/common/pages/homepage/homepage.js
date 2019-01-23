86379576334eaf3e5c866026f2bb23e0266eedbf
lizifa <lphvali@thetiger.com.cn>
2019-01-23 14:02:24 +0800

    修改公司主页

--- page/common/pages/homepage/homepage.js
+++ page/common/pages/homepage/homepage.js
@@ -215,10 +215,12 @@ Page({
    * @detail   查看大图
    * @return   {[type]}   [description]
    */
-  previewImage() {
+  previewImage(e) {
+    const albumInfo = this.data.companyInfos.albumInfo.map(field => field.url)
+    const params = e.currentTarget.dataset
     wx.previewImage({
-      current: '', // 当前显示图片的http链接
-      urls: [] // 需要预览的图片http链接列表
+      current: params.index,
+      urls: albumInfo
     })
   }
 })
