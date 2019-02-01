030217e43bdbe81df82f842e34848b3d44e1de8c
lizifa <lphvali@thetiger.com.cn>
2019-02-01 18:33:39 +0800

    加入公司页面 最后一步跳转 不加反回按钮

--- page/recruiter/pages/user/company/find/find.js
+++ page/recruiter/pages/user/company/find/find.js
@@ -175,7 +175,7 @@ Page({
     }
     applyCompanyApi(params).then(() => {
       // 手机号已经存在 ， 先跳转验证页面
-      wx.redirectTo({url: `${RECRUITER}user/company/status/status?from=apply`})
+      wx.reLaunch({url: `${RECRUITER}user/company/status/status?from=apply`})
       wx.removeStorageSync('createdCompany')
     })
   },
@@ -197,7 +197,7 @@ Page({
     }
     editApplyCompanyApi(params).then(() => {
       // 手机号已经存在 ， 先跳转验证页面
-      wx.redirectTo({url: `${RECRUITER}user/company/status/status?from=apply`})
+      wx.reLaunch({url: `${RECRUITER}user/company/status/status?from=apply`})
       wx.removeStorageSync('createdCompany')
     })
   },
