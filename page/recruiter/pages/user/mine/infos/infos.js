5c3130c5cd5338e6c92c89f02c6e3863b9e2b6a7
lizifa <lphvali@thetiger.com.cn>
2019-02-20 14:44:06 +0800

    修改认证判断字段

--- page/recruiter/pages/user/mine/infos/infos.js
+++ page/recruiter/pages/user/mine/infos/infos.js
@@ -119,7 +119,7 @@ Page({
     
     const identityInfos = this.data.identityInfos
     //还没有填写身份信息
-    if(!identityInfos.status) {
+    if(!identityInfos.identityNum) {
       wx.navigateTo({url: `${RECRUITER}user/company/identity/identity?type=create&realName=${identityInfos.companyInfo.realName}`})
       return;
     }
