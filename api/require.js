6b453a423b43f48c4fec7b70ba93d5fbec688596
WJWM0316 <429029710@QQ.COM>
2019-05-22 20:11:56 +0800

    缩短时间

--- api/require.js
+++ api/require.js
@@ -174,7 +174,7 @@ export const request = ({name = '', method = 'post', url, host, data = {}, needK
                 let timer = setTimeout(() => {
                   toBindPhone = false
                   clearTimeout(timer)
-                }, 3000)
+                }, 2000)
                 wx.removeStorageSync('token')
                 wx.navigateTo({
                   url: `${COMMON}bindPhone/bindPhone`
