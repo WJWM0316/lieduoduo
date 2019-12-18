da042628caeb0d7f8b71fbf0a9b6808469264646
lizifa <lphvali@thetiger.com.cn>
2019-12-18 11:34:26 +0800

    修改

--- components/business/tabBar/tabBar.js
+++ components/business/tabBar/tabBar.js
@@ -1,6 +1,10 @@
 // components/business/tabBar/tabBar.js
 import {RECRUITER, APPLICANT, COMMON} from '../../../config.js'
 import { getBottomRedDotApi } from '../../../api/pages/interview.js'
+import {
+  recruiterList,
+  applicantList
+} from '../../../utils/navigation.js'
 
 const app = getApp()
 const cdnImagePath = app.globalData.cdnImagePath
@@ -20,82 +24,8 @@ Component({
    */
   data: {
     redDot: {},
-    recruiterList: [
-      {
-        title: '首页',
-        flag: 'index',
-        iconPath: `${cdnImagePath}tab_home_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_home_sel@3x.png`,
-        active: true,
-        path: `${RECRUITER}index/index`
-      },
-      {
-        title: '面试',
-        flag: 'interview',
-        iconPath: `${cdnImagePath}tab_interview_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_interview_sel@3x.png`,
-        active: false,
-        path: `${RECRUITER}interview/index/index`
-      },
-      {
-        title: '职位管理',
-        flag: 'position',
-        iconPath: `${cdnImagePath}tab_job_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_job_sel@3x.png`,
-        active: false,
-        path: `${RECRUITER}position/index/index`
-      },
-      {
-        title: '我的',
-        flag: 'mine',
-        iconPath: `${cdnImagePath}tab_me_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_me_sel@3x.png`,
-        active: false,
-        path: `${RECRUITER}user/mine/infos/infos`
-      }
-    ],
-    applicantList: [
-      {
-        title: '发现机会',
-        flag: 'chance',
-        iconPath: `${cdnImagePath}tab_job_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_job_sel@3x.png`,
-        active: true,
-        path: `${APPLICANT}index/index`
-      },
-      {
-        title: '动态',
-        flag: 'dynamics',
-        iconPath: `${cdnImagePath}tab_dynamics.png`,
-        selectedIconPath: `${cdnImagePath}tab_dynamics_active.png`,
-        active: false,
-        path: `${APPLICANT}dynamics/dynamics`
-      },
-      {
-        title: '急速约面',
-        flag: 'specialJob',
-        iconPath: `${cdnImagePath}tab_24h.png`,
-        selectedIconPath: `${cdnImagePath}tab_24h_pre.png`,
-        active: false,
-        path: `${APPLICANT}specialJob/specialJob`
-      },
-      {
-        title: '面试',
-        flag: 'interview',
-        iconPath: `${cdnImagePath}tab_interview_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_interview_sel@3x.png`,
-        active: false,
-        path: `${APPLICANT}interview/interview/interview`
-      },
-      {
-        title: '我的',
-        flag: 'mine',
-        iconPath: `${cdnImagePath}tab_me_nor@3x.png`,
-        selectedIconPath: `${cdnImagePath}tab_me_sel@3x.png`,
-        active: false,
-        path: `${APPLICANT}center/mine/mine`
-      }
-    ],
+    recruiterList,
+    applicantList,
     isIphoneX: app.globalData.isIphoneX
   },
   attached() {
