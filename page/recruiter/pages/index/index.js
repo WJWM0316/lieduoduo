d37f612e455dd381d6ec9bfedb825dd8725d5de3
wuweixian <wwx@thetiger.com.cn>
2020-01-20 20:31:22 +0800

    B端

--- page/recruiter/pages/index/index.js
+++ page/recruiter/pages/index/index.js
@@ -157,7 +157,7 @@ Page({
     }
   },
   initDefaultBar() {
-    // if (!this.data.detail.positionNum) return
+    if (!this.data.detail.positionNum) return
     setTimeout(() => {
       getSelectorQuery('.tab-bar').then(res => {
         let moveParams = this.data.moveParams
@@ -199,7 +199,7 @@ Page({
    * @return   {[type]}   [description]
    */
   getDomNodePosition() {
-    // if (!this.data.detail.positionNum) return
+    if (!this.data.detail.positionNum) return
     setTimeout(() => {
       getSelectorQuery('.default').then(res => {
         if(!fixedDomPosition) fixedDomPosition = res.top - this.data.navH
