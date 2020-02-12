eb1cc169ccca933e4e559403dac97bd62199f81c
wuweixian <wwx@thetiger.com.cn>
2020-02-12 18:30:00 +0800

    下载页H5链接 添加art二级目录名

--- config.js
+++ config.js
@@ -46,4 +46,4 @@ export const COMPONENTS = '/components/'
 export const API = '/api/'
 
 // downLoadApp
-export const DOWNLOADAPPPATH = `${COMMON}webView/webView?p=${encodeURIComponent(`${WEBVIEW}downLoadApp?sourceType=miniGuide`)}`
+export const DOWNLOADAPPPATH = `${COMMON}webView/webView?p=${encodeURIComponent(`${WEBVIEW}art/downLoadApp?sourceType=miniGuide`)}`
